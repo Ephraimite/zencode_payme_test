2c7f4f7b539d2bd2b2344cec0c4752f2c965c877
Ephrain Ibatt <aniekanephraim@gmail.com>
2025-02-03 07:49:14 +0100

    shaking off

--- lib/feature/dashboard/presentation/view/dashboard_view.dart
+++ lib/feature/dashboard/presentation/view/dashboard_view.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -92,7 +91,7 @@ class _DashboardContent extends StatelessWidget {
                           ),
                         ),
                       ),
-                      Spacer(),
+                      const Spacer(),
                     ],
                     Container(
                       decoration: BoxDecoration(
